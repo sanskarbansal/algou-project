a6f6a25de6dfedac8a641b09696595f4ac5cc2ec
Sanskar <sanskarbnsl75@gmail.com>
2022-06-28 15:54:43 +0530

    Feat: add Loader

--- frontend/src/components/Problem/Problem.js
+++ frontend/src/components/Problem/Problem.js
@@ -10,14 +10,17 @@ import { useState } from "react";
 import CodeEditor from "../CodeEditor";
 import classNames from "classnames/bind";
 import styles from "./problem.module.css";
+import Loader from "../Loader/Loader";
 
 const cx = classNames.bind(styles);
 
 export default function Problem() {
     const [problem, setProblem] = useState({});
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
     const { problemId } = useParams();
     useEffect(() => {
+        setLoading(true);
         axios
             .get(`${config.backendEndpoint}/v1/problems/${problemId}`)
             .then((resp) => {
@@ -28,30 +31,41 @@ export default function Problem() {
                 if (error.response.data.code >= 400 && error.response.data.code < 500) {
                     setError(error.response.data);
                 }
+            })
+            .finally(() => {
+                setLoading(false);
             });
     }, [problemId]);
 
     const { title, difficulty, problem: body } = problem;
 
     return (
-        <div className={cx("problem-container")}>
-            <Link to="/">&lt; Back </Link>
-            {title && (
-                <>
-                    <div className={cx("problem-heading")}>
-                        <h1 className={cx("problem-title")}>{title}</h1>
-                        <h5 className={cx("problem-difficulty", `problem-difficulty_${difficulty?.toLowerCase()}`)}>{difficulty}</h5>
-                    </div>
-                    <div className={cx("problem-body")}>
-                        <Markdown content={body} />
-                    </div>
-                    <h1>Code Editor</h1>
-                    <div className={cx("problem-editor")}>
-                        <CodeEditor />
-                    </div>
-                </>
+        <>
+            {loading ? (
+                <div className={cx("center")}>
+                    <Loader />
+                </div>
+            ) : (
+                <div className={cx("problem-container")}>
+                    <Link to="/">&lt; Back </Link>
+                    {title && (
+                        <>
+                            <div className={cx("problem-heading")}>
+                                <h1 className={cx("problem-title")}>{title}</h1>
+                                <h5 className={cx("problem-difficulty", `problem-difficulty_${difficulty?.toLowerCase()}`)}>{difficulty}</h5>
+                            </div>
+                            <div className={cx("problem-body")}>
+                                <Markdown content={body} />
+                            </div>
+                            <h1>Code Editor</h1>
+                            <div className={cx("problem-editor")}>
+                                <CodeEditor />
+                            </div>
+                        </>
+                    )}
+                    {error && <h1 className={cx("error-title")}>{error.message}</h1>}
+                </div>
             )}
-            {error && <h1 className={cx("error-title")}>{error.message}</h1>}
-        </div>
+        </>
     );
 }
--- frontend/src/components/ProblemList/ProblemsList.js
+++ frontend/src/components/ProblemList/ProblemsList.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import config from "../../config.json";
-import ProblemItem from "./ProblemItem";
 import classNames from "classnames/bind";
+import axios from "axios";
+import ProblemItem from "./ProblemItem";
+import Loader from "../Loader/Loader";
+import config from "../../config.json";
 import styles from "./problemList.module.css";
 const cx = classNames.bind(styles);
 
 export default function ProblemsList() {
     const [problems, setProblems] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
+        setLoading(true);
         axios
             .get(`${config.backendEndpoint}/v1/problems`)
             .then((resp) => {
@@ -16,17 +19,28 @@ export default function ProblemsList() {
             })
             .catch((error) => {
                 alert(error.response.data);
+            })
+            .finally(() => {
+                setLoading(false);
             });
     }, []);
 
     return (
-        <div className={cx("problemList-container")}>
-            <h1 className={cx("problemList-heading")}>Problem List</h1>
-            <div className={cx("problemList-problems")}>
-                {problems.map((problem) => (
-                    <ProblemItem key={problem._id} {...problem} />
-                ))}
-            </div>
-        </div>
+        <>
+            {loading ? (
+                <div className={cx("center")}>
+                    <Loader />
+                </div>
+            ) : (
+                <div className={cx("problemList-container")}>
+                    <h1 className={cx("problemList-heading")}>Problem List</h1>
+                    <div className={cx("problemList-problems")}>
+                        {problems.map((problem) => (
+                            <ProblemItem key={problem._id} {...problem} />
+                        ))}
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
